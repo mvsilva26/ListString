b0cf0b5a0aca567a31c9c10b60e9f7a45f3c61dd
Michael Silva <michael.steam@outlook.com>
2022-03-08 09:10:12 -0300

    Adicionar arquivos de projeto.

---
+++ Ex3List/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex3List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> listaString = new List<string>();
+
+            
+
+            Console.WriteLine("Capacidade: " + listaString.Capacity);
+            string nome;
+            
+            do
+            {
+                Console.WriteLine("Digite um nome pra inserir na lista");
+                nome = Console.ReadLine();
+                listaString.Add(nome);
+
+            } while (nome != "");
+            
+            listaString.ForEach(i => Console.WriteLine(i));
+
+        }
+    }
+}
